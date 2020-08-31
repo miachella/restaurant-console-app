1bda332f0948f577cceff81c9bca438011a43496
Mathilde IACHELLA <mathilde.iachella@gmail.com>
2020-08-31 15:52:04 +0200

    TP 1.2

--- src/main/java/dev/ihm/Menu.java
+++ src/main/java/dev/ihm/Menu.java
@@ -11,6 +11,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class Menu {
 
     private Map<Integer, IOptionMenu> actions = new HashMap<>();
