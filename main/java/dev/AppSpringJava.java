639f35f980b23a599a21638d4e414e213799f9a7
Mathilde IACHELLA <mathilde.iachella@gmail.com>
2020-09-01 10:40:01 +0200

    TP 3.1

---
+++ main/java/dev/AppSpringJava.java
@@ -0,0 +1,24 @@
+package dev;
+
+import java.util.Scanner;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import dev.config.AppConfig;
+import dev.ihm.MenuBean;
+
+public class AppSpringJava {
+	
+	public static void main(String[] args) {
+		// Création du contexte Spring à partir d'une configuration Java
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		// récupération du bean Menu
+		MenuBean menu = context.getBean(MenuBean.class);
+		menu.afficher();
+		// fermeture du Scanner
+		context.getBean(Scanner.class).close();
+		// fermeture du contexte Spring
+		context.close();
+		}
+
+}
